3495b3716693a0db0f0de060d2fc280d0ff14f19
Ricardo <Zed@172.20.8.67>
2020-11-22 09:30:06 +0100

    Waves Keeper Auth integrated

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,31 @@
 import ReactDOM from "react-dom";
 import React from 'react';
+
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {}
+        this.authFunc = this.authFunc.bind(this);
+    }
+    authFunc() {
+        const authData = { data: "Autorizar en mi sitio" };
+        if (WavesKeeper) {
+            WavesKeeper.auth( authData )
+            .then(auth => {
+                console.log( auth ); // muestra los resultados en la consola
+                /*...procesando la informacion */
+            }).catch(error => {
+                console.error( error ); // muestra los resultados en la consola
+                /*...procesando errores */
+            })
+        } else {
+            alert("Para autorizar, WavesKeeper debe estar instalado.");
+        }
     }
     render() {
         return (
             <div className="container">
-    		    <input className="btn btn-primary" type="submit" value="Alert" onClick={() => {alert("Alert button onClick");}}/>
+    		    <input className="btn btn-primary" type="submit" value="Auth" onClick={this.authFunc}/>
     	    </div>
         )
     }
